b0c33205f73ba15bd691f7dcf7ffa8cd7312d51e
wahyu6070 <gwtsendangrejo6070@gmail.com>
2021-06-22 23:04:24 +0700

    improv

--- unpack.sh
+++ unpack.sh
@@ -6,35 +6,17 @@
 #PATH
 jancox=`dirname "$(readlink -f $0)"`
 #functions
-. $jancox/bin/arm/kopi
-#bin
-bin=$jancox/bin/$ARCH32
-bb=$bin/busybox
-tmp=$jancox/bin/tmp
-pybin=$jancox/bin/python
-editor=$jancox/editor
-log=$jancox/bin/jancox.log
-loglive=$jancox/bin/jancox.live.log
+. $jancox/bin/jancox_functions
 chmod -R 755 $bin
 [ $(pwd) != $jancox ] && cd $jancox
-del $loglive && touch $loglive
-[ -d $tmp ] && del $tmp
-[ -d $editor ] && del $editor
-cdir $tmp
-cdir $editor
-if [ -f /data/data/com.termux/files/usr/bin/python ]; then
-py=/data/data/com.termux/files/usr/bin/python
-else
-printlog " "
-printlog "$- python 3 Not Installed In Termux !"
-printlog " "
-printlog "- apt update"
-printlog "- apt upgrade"
-printlog "- pkg install python"
-printlog " "
-sleep 1s
-exit
-fi
+
+for W777 in $TMP $EDITOR; do
+ [ -d $W777 ] && del $W777 && cdir $W777 || cdir $W777
+done
+
+#
+check_python
+
 #input.zip
 for ajax in $jancox /data/media/0 /data/media/0/Download; do
      if [ -f $ajax/input.zip ]; then
@@ -43,8 +25,9 @@ for ajax in $jancox /data/media/0 /data/media/0/Download; do
      fi;
 done
 clear
-[ ! -f $jancox/credits.txt ] && printlog "    Credits not found !!" | sleep 3s | exit ;
-
+if [ ! -f $jancox/credits.txt ]; then
+ERROR "Please dont delete credits"
+fi
 printmid "${Y}Jancox Tool by wahyu6070${W}"
 printlog " "
 printlog "       Unpack"
@@ -58,86 +41,103 @@ fi
 
 if [ $input ]; then
 printlog "- Extracting input.zip..."
-$bin/unzip -o $input -d $tmp >> $loglive
-listlog "$tmp"
+$BIN/unzip -o $input -d $TMP >> $LOG
+listlog "$TMP"
 fi
 
-if [ -f $tmp/*.bin ]; then
+if [ -f $TMP/*.bin ]; then
 printlog "- Extracting Payload.bin"
-$py $pybin/payload_dumper.py $tmp/*.bin --out $tmp >> $loglive
+$PY $PYBIN/payload_dumper.py $TMP/*.bin --out $TMP >> $LOG
 payloadbin=true
 fi
 
-if [ -f $tmp/system.new.dat.br ]; then
+if [ -f $TMP/system.new.dat.br ]; then
 printlog "- Extraction system.new.dat.br... "
-$bin/brotli -d $tmp/system.new.dat.br -o $tmp/system.new.dat
-del $tmp/system.new.dat.br $tmp/system.patch.dat
+$BIN/brotli -d $TMP/system.new.dat.br -o $TMP/system.new.dat
+del $TMP/system.new.dat.br $TMP/system.patch.dat
 fi
 
-if [ -f $tmp/vendor.new.dat.br ]; then
+if [ -f $TMP/product.new.dat.br ]; then
+printlog "- Extraction product.new.dat.br... "
+$BIN/brotli -d $TMP/system.new.dat.br -o $TMP/system.new.dat
+del $TMP/product.new.dat.br $TMP/product.patch.dat
+fi
+
+if [ -f $TMP/vendor.new.dat.br ]; then
 printlog "- Extraction vendor.new.dat.br... "
-$bin/brotli -d $tmp/vendor.new.dat.br -o $tmp/vendor.new.dat
-del $tmp/vendor.new.dat.br $tmp/vendor.patch.dat
+$BIN/brotli -d $TMP/vendor.new.dat.br -o $TMP/vendor.new.dat
+del $TMP/vendor.new.dat.br $TMP/vendor.patch.dat
 fi
 
-if [ -f $tmp/system.new.dat ]; then
+if [ -f $TMP/system.new.dat ]; then
 printlog "- Extraction system.new.dat... "
-$py $pybin/sdat2img.py $tmp/system.transfer.list $tmp/system.new.dat $tmp/system.img >> $loglive
-del $tmp/system.new.dat $tmp/system.transfer.list
+$PY $PYBIN/sdat2img.py $TMP/system.transfer.list $TMP/system.new.dat $TMP/system.img >> $LOG
+del $TMP/system.new.dat $TMP/system.transfer.list
 fi
 
-if [ -f $tmp/vendor.new.dat ]; then
+if [ -f $TMP/product.new.dat ]; then
+printlog "- Extraction product.new.dat... "
+$PY $PYBIN/sdat2img.py $TMP/product.transfer.list $TMP/product.new.dat $TMP/product.img >> $LOG
+del $TMP/product.new.dat $TMP/product.transfer.list
+fi
+
+if [ -f $TMP/vendor.new.dat ]; then
 printlog "- Extraction vendor.new.dat... "
-$py $pybin/sdat2img.py $tmp/vendor.transfer.list $tmp/vendor.new.dat $tmp/vendor.img >> $loglive
-del $tmp/vendor.new.dat $tmp/vendor.transfer.list
+$PY $PYBIN/sdat2img.py $TMP/vendor.transfer.list $TMP/vendor.new.dat $TMP/vendor.img >> $LOG
+del $TMP/vendor.new.dat $TMP/vendor.transfer.list
 fi
 
-[ -d $jancox/editor ] && rm -rf $editor
-mkdir -p $editor
-if [ -f $tmp/system.img ]; then
+if [ -f $TMP/system.img ]; then
 printlog "- Extraction system.img... "
-$py $pybin/imgextractor.py $tmp/system.img $editor/system >> $loglive
-del $tmp/system.img
+$PY $PYBIN/imgextractor.py $TMP/system.img $EDITOR/system >> $LOG
+del $TMP/system.img
 fi
 
-if [ -f $tmp/vendor.img ]; then
+if [ -f $TMP/product.img ]; then
+printlog "- Extraction product.img... "
+mv $TMP/product.img $TMP/system.img
+$PY $PYBIN/imgextractor.py $TMP/system.img $EDITOR/product >> $LOG
+del $TMP/system.img
+fi
+
+if [ -f $TMP/vendor.img ]; then
 printlog "- Extraction vendor.img... "
-$py $pybin/imgextractor.py $tmp/vendor.img $editor/vendor >/dev/null
-del $tmp/vendor.img
+$py $pybin/imgextractor.py $TMP/vendor.img $EDITOR/vendor >/dev/null
+del $TMP/vendor.img
 fi
 
-if [ -f $tmp/boot.img ]; then
+if [ -f $TMP/boot.img ]; then
 printlog "- Extraction boot.img"
-$bin/magiskboot unpack $tmp/boot.img  2>> $loglive
-cdir $editor/boot
+$bin/magiskboot unpack $TMP/boot.img  2>> $LOG
+cdir $EDITOR/boot
 for MV_BOOT in ramdisk.cpio kernel kernel_dtb dtb second; do
-[ -f $jancox/$MV_BOOT ] && mv -f $jancox/$MV_BOOT $editor/boot/
-sedlog "- Moving boot file $jancox/$MVBOOT to $editor/boot/"
+[ -f $jancox/$MV_BOOT ] && mv -f $jancox/$MV_BOOT $EDITOR/boot/
+sedlog "- Moving boot file $jancox/$MVBOOT to $EDITOR/boot/"
 done
 fi
 
-[ -d $tmp/META-INF ] && mv -f $tmp/META-INF $editor
-[ -d $tmp/system ] && mv -f $tmp/system $editor/system2
-[ -d $tmp/firmware-update ] && mv -f $tmp/firmware-update $editor
-[ -d $tmp/install ] && mv -f $tmp/install $editor
-[ -f $tmp/boot.img ] && mv $tmp/boot.img $editor/boot
-[ -f $tmp/compatibility.zip ] && mv $tmp/compatibility.zip $editor
-[ -f $tmp/system_file_contexts ] && mv -f $tmp/system_file_contexts $editor/system_file_contexts
-[ -f $tmp/vendor_file_contexts ] && mv -f $tmp/vendor_file_contexts $editor/vendor_file_contexts
-[ -f $tmp/system_fs_config ] && mv -f $tmp/system_fs_config $editor/system_fs_config
-[ -f $tmp/vendor_fs_config ] && mv -f $tmp/vendor_fs_config $editor/vendor_fs_config
+[ -d $TMP/META-INF ] && mv -f $TMP/META-INF $EDITOR
+[ -d $TMP/system ] && mv -f $TMP/system $EDITOR/system2
+[ -d $TMP/firmware-update ] && mv -f $TMP/firmware-update $EDITOR
+[ -d $TMP/install ] && mv -f $TMP/install $EDITOR
+[ -f $TMP/boot.img ] && mv $TMP/boot.img $EDITOR/boot
+[ -f $TMP/compatibility.zip ] && mv $TMP/compatibility.zip $EDITOR
+[ -f $TMP/system_file_contexts ] && mv -f $TMP/system_file_contexts $EDITOR/system_file_contexts
+[ -f $TMP/vendor_file_contexts ] && mv -f $TMP/vendor_file_contexts $EDITOR/vendor_file_contexts
+[ -f $TMP/system_fs_config ] && mv -f $TMP/system_fs_config $EDITOR/system_fs_config
+[ -f $TMP/vendor_fs_config ] && mv -f $TMP/vendor_fs_config $EDITOR/vendor_fs_config
 
-test $payloadbin && del $tmp
+test $payloadbin && del $TMP
 
-if [ -f $editor/system/build.prop ]; then
+if [ -f $EDITOR/system/build.prop ]; then
 printlog "- Done "
 printlog " "
-rom-info $editor > $editor/rom-info
-elif [ -f $editor/system/system/build.prop ]; then
+rom-info $EDITOR > $editor/rom-info
+elif [ -f $EDITOR/system/system/build.prop ]; then
 printlog "- Done"
 printlog " "
-rom-info $editor > $editor/rom-info
-cat $editor/rom-info
+rom-info $EDITOR > $editor/rom-info
+cat $EDITOR/rom-info
 else
 printlog "- Finished with the problem"
 fi
